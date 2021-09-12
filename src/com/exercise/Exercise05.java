3b6c2609dfc9d54f893ec42144d80307cb95e7ce
GianniAngelone <angelone.gianni@gmail.com>
2021-09-12 18:16:29 -0400

    Initial commit

---
+++ src/com/exercise/Exercise05.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Gianni Angelone
+ */
+package com.exercise;
+import java.util.Scanner;
+class Exercise05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Here we take in the input from a user
+        System.out.print("What is the first number? ");
+        String Number1 = input.nextLine();
+        System.out.print("What is the second number? ");
+        String Number2 = input.nextLine();
+        //Since we can't do math with strings, we have to convert it
+        int a = Integer.parseInt(Number1);
+        int b = Integer.parseInt(Number2);
+        //Last we print the calculations.
+        System.out.println(String.format("%d + %d = %d", a,b, a+b));
+        System.out.println(String.format("%d - %d = %d", a,b, a-b));
+        System.out.println(String.format("%d * %d = %d", a,b, a*b));
+        System.out.println(String.format("%d / %d = %d", a,b, a/b));
+    }
+}
